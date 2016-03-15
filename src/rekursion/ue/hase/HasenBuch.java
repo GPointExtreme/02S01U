4d4d37075d4b595c6a3dadf153afe1025c204662
GPointExtreme <dominik.geiger@hotmail.com>
2016-03-15 18:42:17 +0100

    Rekursion üben. Hart am Limit!

---
+++ src/rekursion/ue/hase/HasenBuch.java
@@ -0,0 +1,23 @@
+package rekursion.ue.hase;
+
+public class HasenBuch {
+
+	public static void main(String[] args) {
+		lesen(3);
+
+	}
+	
+	public static void lesen (int tiefe) {
+		if (tiefe == 0) {
+			System.out.println("Ein Hase, ...");
+			return;
+		}
+		System.out.println("Ein Hase, der gern Bücher las, fand ein dickes Buch im Gras,");
+		System.out.println("er setzte sich ins Gras und las das dicke Buch,");
+		System.out.println("im Buch stand das:");
+		tiefe--;
+		lesen(tiefe);
+		
+	}
+
+}
